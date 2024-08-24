96ddda50b3f94a661f2fe48ef53b68d6792b72a7
Pham Truong Giang <ptgiang.dora@gmail.com>
2024-08-24 18:07:54 +0700

    Updated button previous and next in orders view admin

--- app/views/admin/includes/main_orders.php
+++ app/views/admin/includes/main_orders.php
@@ -100,10 +100,10 @@
                 <!-- .... -->
             </tbody>
         </table>
-    </div>
-    <div class="d-flex justify-content-between">
-        <button id="prevPageBtn" class="btn btn-outline-secondary btn-sm">Previous</button>
-        <button id="nextPageBtn" class="btn btn-outline-secondary btn-sm">Next</button>
+        <div class="d-flex justify-content-between">
+            <button id="prevPageBtn" class="btn btn-outline-secondary btn-sm">Previous</button>
+            <button id="nextPageBtn" class="btn btn-outline-secondary btn-sm">Next</button>
+        </div>
     </div>
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
     </div>
